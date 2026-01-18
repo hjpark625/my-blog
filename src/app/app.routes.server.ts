7d2279e3f1219b9966c10629762387d7c8eaf4cc
hjpark625 <zero950@gmail.com>
2026-01-18 18:41:05 +0900

    chore: Configure Angular app structure and styles
    - Setup TailwindCSS
    - Configure routes and SSR settings
    - Update app config

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'blog/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'edit/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'write',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './features/blog/post-list/post-list';
+import { PostDetailComponent } from './features/blog/post-detail/post-detail';
+import { PostEditorComponent } from './features/blog/post-editor/post-editor';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'blog', pathMatch: 'full' },
+  { path: 'blog', component: PostListComponent },
+  { path: 'blog/:id', component: PostDetailComponent },
+  { path: 'write', component: PostEditorComponent },
+  { path: 'edit/:id', component: PostEditorComponent },
+];
