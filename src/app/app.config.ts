33beb6b5c56fde30b85cdfc447913d1fb3afc441
hjpark625 <zero950@gmail.com>
2026-01-18 19:12:25 +0900

    chore: Set application locale to Korean (ko-KR)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import localeKo from '@angular/common/locales/ko';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { provideMarkdown } from 'ngx-markdown';
+
+registerLocaleData(localeKo);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +16,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     provideClientHydration(withEventReplay()),
     provideMarkdown(),
+    { provide: LOCALE_ID, useValue: 'ko-KR' },
   ],
 };
